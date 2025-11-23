54e24edb461c40a16a9adea6eb775011ba012a0a
DayaMathew <dayamathew29@gmail.com>
2025-11-23 15:30:36 +0530

    Add files via upload

---
+++ program-14.c
@@ -0,0 +1,29 @@
+/* Program to read a string and reverse the string in the same array itself
+   Daya Mathew
+   29 July 2025
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main()
+ {
+    char str[100];
+    int i, j;
+    char temp;
+
+    printf("Enter a string: ");
+    scanf("%s", str); 
+
+    i = 0;
+    j = strlen(str) - 1;
+
+    while (i < j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+
+    printf("Reversed string: %s\n", str);
+}
